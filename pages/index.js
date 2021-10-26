1e500d70820bafd61c8fd518197a2709335ec421
entornoInteractivo <cristianvelasco1529@gmail.com>
2021-10-26 16:39:03 -0500

    first commit app

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>Shopify app with Node and React 🎉 ok ok</Heading>
+  </Page>
+);
+
+export default Index;
